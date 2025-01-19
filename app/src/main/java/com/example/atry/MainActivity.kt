3be64621b2f0d3a4769a4b134608141fdebbf386
Naval <navaldhiman30@gmail.com>
2025-01-19 19:33:14 +0530

    added name naman

--- app/src/main/java/com/example/atry/MainActivity.kt
+++ app/src/main/java/com/example/atry/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TryTheme {
-        Greeting("Android //Naval//harsh//gaur//krish")
+        Greeting("Android //Naval//harsh//gaur//krish//naman")
     }
 }
